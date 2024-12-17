275d0f19d53602ff6bbab194c59474d8641b520f
LianChia <s20s20wuxing@gmail.com>
2024-12-17 16:38:22 +0800

    refactor：更改圖片路徑

--- tests/test_dentalMeasure.py
+++ tests/test_dentalMeasure.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 import cv2
 from src.dental_measure.main import *
 def test_dentalEstimation_normalImage():
-    image=cv2.imread('C:/Users/SSTek/Desktop/LianChia/dentistry-inference-fastapi-main/tests/nomal-x-ray-0.8510638-270-740_0_2022011008.png')
+    image=cv2.imread('./tests/nomal-x-ray-0.8510638-270-740_0_2022011008.png')
     if image is None:
         raise ValueError("Image not found, check test image path (or utf8 problems) or cv2 package")
     results_list=dental_estimation(image, scale=(31/960,41/1080), return_type='dict')
@@ -12,7 +12,7 @@ def test_dentalEstimation_normalImage():
         raise ValueError("When input normal image, the result should be found, plz check dental_estimation function")
 
 def test_dentalEstimation_blackimage():
-    image=cv2.imread('C:/Users/SSTek/Desktop/LianChia/dentistry-inference-fastapi-main/tests/black.png')
+    image=cv2.imread('./tests/black.png')
     if image is None:
         raise ValueError("Image not found, check test image path (or utf8 problems) or cv2 package")
     results_list=dental_estimation(image, scale=(31/960,41/1080), return_type='dict')
